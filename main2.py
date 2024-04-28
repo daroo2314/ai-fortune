721bf91d06576cbf0f4cbd4d3c0b6a9015133f7e
ondal <74591368+daroo2314@users.noreply.github.com>
2024-04-28 18:38:37 +0900

    Update main2.py

--- main2.py
+++ main2.py
@@ -5,16 +5,16 @@ from langchain.chat_models import ChatOpenAI
 
 chat_model = ChatOpenAI()
 st.balloons()
-st.title('AI fortune teller')
+st.title('Jenny's AI fortune teller')
 
-content = st.text_input('묻고싶은 내용을 입력하세요')
-st.text('원하는 답변이 아니면 버튼을 다시 눌러주세요')
-# result = chat_model.predict(content + "국악가사를 써줘")
+content = st.text_input('Please enter your date of birth')
+st.text('If the answer you want is not correct, please press the button again.')
+   result = chat_model.predict('My birthdate is' + content + 'Tell me today's fortune. The phrase that starts with' +content+ 'is your fortune that was born in' + content)
 # print(result)
 
-if st.button("답해줘"):
-   with st.spinner('작성중...'):
+if st.button("Tell me please"):
+   with st.spinner('Reading the fortune...'):
       result = chat_model.predict(content)
       st.write(result) 
-st.image('https://oasisbpartners.netlify.app/assets/images/resources/logo.png')
+st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRabYvzb-gX147vBNH-entQ4v6hkwnydLPsC0qepYtIua0eNBhmpEc_REG1WEcckFlVYmI&usqp=CAU')
 #st.link_button("Go to Roar Festival", "http://daroousa.com/en/bbs/board.php?bo_table=en_w4_c&wr_id=1")
