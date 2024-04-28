166c6ac14a5a38ce52a04f3c1789f41a82a7ce58
ondal <74591368+daroo2314@users.noreply.github.com>
2024-04-28 19:16:13 +0900

    Update main2.py

--- main2.py
+++ main2.py
@@ -9,12 +9,12 @@ st.title("Jenny's AI fortune teller")
 
 content = st.text_input('Please enter your date of birth(yyyy-mm-dd)')
 st.text('If the answer you want is not correct, please press the button again.')
-result = chat_model.predict("생년월일이" + content+"인데 오늘의 운세를 말해줘 영어로 번역해서 답해줘")
+#result = chat_model.predict("생년월일이"+content+"인데 오늘의 운세를 말해줘 영어로 번역해서 답해줘")
 # print(result)
 
 if st.button("Tell me please"):
    with st.spinner('Reading the fortune...'):
-      result = chat_model.predict(content)
+      result = chat_model.predict("생년월일이"+content+"인데 오늘의 운세를 말해줘 영어로 번역해서 답해줘")
       st.write(result) 
 st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRabYvzb-gX147vBNH-entQ4v6hkwnydLPsC0qepYtIua0eNBhmpEc_REG1WEcckFlVYmI&usqp=CAU')
 #st.link_button("Go to Roar Festival", "http://daroousa.com/en/bbs/board.php?bo_table=en_w4_c&wr_id=1")
